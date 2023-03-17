6ef5b72c78863f3cbe38b7aba9e6bb1a7e2e1a01
i3ym <deniso99@mail.ru>
2023-03-17 23:07:58 +0700

    Add DefaultLogging

---
+++ Common/DefaultLogging.cs
@@ -0,0 +1,26 @@
+using NLog.Targets;
+
+namespace Zomlib;
+
+public static class DefaultLogging
+{
+    public static void Setup()
+    {
+        const string logLayout = "${date:format=HH\\:mm\\:ss:universalTime=true} [${level:uppercase=true} @ ${logger:shortName=true}] ${message:withException=true:exceptionSeparator=\n\n}";
+
+        LogManager.AutoShutdown = true;
+        LogManager.GlobalThreshold = LogLevel.Trace;
+        LogManager.Setup()
+            .SetupLogFactory(config => config.SetTimeSourcAccurateUtc())
+            .LoadConfiguration(setup => setup.ForLogger()
+                .WriteTo(new ColoredConsoleTarget()
+                {
+                    DetectConsoleAvailable = true,
+                    Layout = logLayout,
+                    AutoFlush = true,
+                    UseDefaultRowHighlightingRules = true,
+                })
+            );
+
+    }
+}
